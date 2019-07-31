dc3fec77c6d8467c4a5bc6d7f4bdfb64e7584d5a
Aaron Mak <aaronmakks@gmail.com>
2019-07-31 08:03:50 +0800

    Fix folder path and silence prompt

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "Creating repository directories..."
-mkdir -p code/personal
-mkdir -p code/work
-mkdir -p code/others
+mkdir -p ~/code/personal
+mkdir -p ~/code/work
+mkdir -p ~/code/others
 
 echo "Updating apt-get..."
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
