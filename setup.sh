0bdd25ce8d8c32121abb860c552defaa5fcfd620
Aaron Mak <aaronmakks@gmail.com>
2019-07-31 08:47:24 +0800

    Add essential packages

--- setup.sh
+++ setup.sh
@@ -8,3 +8,14 @@ mkdir -p ~/code/others
 echo "Updating apt-get..."
 sudo apt-get -y update
 sudo apt-get -y upgrade
+
+echo "Installing packages..."
+
+echo "Installing essentials..."
+sudo apt-get install -y build-essential
+sudo apt-get install -y moreutils
+sudo apt-get install -y findutils
+sudo apt-get install -y curl
+sudo apt-get install -y file
+sudo apt-get install -y git
+sudo apt-get install -y tmux
