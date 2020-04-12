d6e556a32a1aab9af09e82604b4690fe453c6fd4
Aaron Mak <aaronmakks@gmail.com>
2020-04-12 02:47:22 +0000

    Add tmux plugin manager

--- setup.sh
+++ setup.sh
@@ -65,6 +65,12 @@ git checkout "$(git describe --abbrev=0 --tags)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}" || exit
 
+########################
+# Tmux Plugin Manager  #
+########################
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 #############
 # Dotfiles  #
 #############
